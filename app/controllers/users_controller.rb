b29a689d1c27fef6e0d5fae7c2dee9f452951812
aljimenez5 <arvida.jimenez@gmail.com>
2019-10-14 14:28:54 -0400

    create new user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,30 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        user = User.find_by(email: params[:email])
+        if user 
+            redirect_to user_path(user)
+        else 
+            user = User.create(user_params)
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        end
     end
 
     def show
+        if logged_in?
+            @user = User.find(session[:user_id])
+        else
+            redirect_to log_in_path
+        end 
 
-    end 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 
 
 end
