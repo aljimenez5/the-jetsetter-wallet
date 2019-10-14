bc7c0347655e07eb0314e9bea1690ceae5187753
aljimenez5 <arvida.jimenez@gmail.com>
2019-10-14 14:06:18 -0400

    set up sign up and log in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root to: "homepage#index"
+  root to: "countries#index"
 
+  get '/sign_up' => "users#new"
+  post '/sign_up' => "users#create"
+  get '/log_in' => "sessions#new"
+  post '/log_in' => "sessions#create"
+  resources :users, only: [:destroy]
+  resources :sessions, only: [:destroy]
+  
   resources :users, only: [:show] do
     resources :trips do
       resources :activities
