b18c25201450fc50e38ee2ee55908bebf9495c48
aljimenez5 <arvida.jimenez@gmail.com>
2019-10-13 23:36:40 -0400

    updated relationships between city and country

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,5 @@ class City < ApplicationRecord
     belongs_to :country
     has_many :trips
     has_many :activities
+    has_many :users, through: :trips
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,4 @@
 class Country < ApplicationRecord
     has_many :cities
     has_many :trips, through: :cities
-    has_many :users, through: :trips
 end
